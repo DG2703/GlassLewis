397cb061786c9ab9841d95c126cc5e1ea08c8455
DG2703 <41378203+DG2703@users.noreply.github.com>
2018-11-12 21:49:20 +0000

    Add files via upload

---
+++ Models/mvcCompanyModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Models
+{
+    public class MvcCompanyModel
+    {
+
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, 
+            ErrorMessage = "Company Name is Required.")]
+        public string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false,
+            ErrorMessage = "StockTicker is Required.")]
+        public string StockTicker { get; set; }
+
+        [Required(AllowEmptyStrings = false,
+            ErrorMessage = "Exchange is Required.")]
+        public string Exchange { get; set; }
+
+        [Key]
+        [DisplayName("ISIN")]
+        [Required(AllowEmptyStrings = false,
+            ErrorMessage = "Company ISIN is Required.")]
+        [RegularExpression(@"^[A-Za-z]{2}[a-zA-Z0-9\s]+$",
+            ErrorMessage = "The ISIN first two charcacters MUST be two alphabetic characters.")]
+        //[Remote("IsISIN_Available", "CompanyController", ErrorMessage = "ISIN Already Exist.")]
+        public string Isin { get; set; }
+
+        public string Url { get; set; }
+
+
+    }
+}
